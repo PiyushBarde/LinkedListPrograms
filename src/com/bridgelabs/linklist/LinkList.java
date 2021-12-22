1c1f06a5ac51574a04ccce11d21b8abfd2831680
PiyushBarde <piyushbarde1@gmail.com>
2021-12-22 19:57:11 +0530

    UPDATED: ability to search node UC7

--- src/com/bridgelabs/linklist/LinkList.java
+++ src/com/bridgelabs/linklist/LinkList.java
@@ -3,32 +3,33 @@ package com.bridgelabs.linklist;
 public class LinkList {
     static Node head;
 
-    public  void appendingMethod(int key)
+    public  Node appendingMethod(int key)
     {
         Node newNode = new Node(key);
         if(head==null){
             head=newNode;
-            return;
+            return null;
         }
         Node currNode =head;
         while(currNode.next!=null){
             currNode=currNode.next;
         }
         currNode.next=newNode;
+        return newNode;
     }
-    public static void deleteLast(){
-        if(head==null){
-            System.out.println("List is empty");
-            return;
-        }
-        Node secondLast = head;
-        Node currNode = head.next;
-        while (currNode.next!=null){
-            currNode=currNode.next;
-            secondLast=secondLast.next;
-        }
-        secondLast.next = null;
-    }
+//    public static void deleteLast(){
+//        if(head==null){
+//            System.out.println("List is empty");
+//            return;
+//        }
+//        Node secondLast = head;
+//        Node currNode = head.next;
+//        while (currNode.next!=null){
+//            currNode=currNode.next;
+//            secondLast=secondLast.next;
+//        }
+//        secondLast.next = null;
+//   }
     static void printLinkList(){
         if( head == null )
         {
@@ -43,13 +44,23 @@ public class LinkList {
       System.out.println(currNode.key);
     }
 
+    static void findElement(){
+        int counter = 1;
+        while(head.key!=30){
+            counter++;
+            head=head.next;
+        }
+        System.out.println("Node "+ counter + " is equal to 30");
+    }
+
     public static void main(String[] args) {
         LinkList linkList = new LinkList();
         linkList.appendingMethod(56);
         linkList.appendingMethod(30);
         linkList.appendingMethod(70);
-        deleteLast();
+
         printLinkList();
+        findElement();
 
     }
 }
